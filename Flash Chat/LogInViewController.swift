b1269ae4dbf3b2084d4c71e380dca5b72ccc6ae8
mlutfiibra <mlutfiibra@outlook.com>
2019-10-22 02:29:08 +0700

    Loadin in login

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
@@ -25,7 +26,8 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
-
+        
+        SVProgressHUD.show()
         
         //TODO: Log in the user
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (authUser, error) in
@@ -33,6 +35,9 @@ class LogInViewController: UIViewController {
             if(error != nil) {
                 print(error!)
             } else {
+                
+                SVProgressHUD.dismiss()
+                
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
             
